21110c2df6a71e190a2fafde299425bc7b5e55d2
Leon Fagner <leonfagnerr@gmail.com>
2024-06-23 16:12:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function projetos(){
+    let vh = 200; 
+    window.scrollTo({
+            top: vh * window.innerHeight / 100, 
+             behavior: 'smooth'
+           });
+
+}function tecnologias(){
+    vh=100;
+    window.scrollTo({
+            top: vh * window.innerHeight / 100, 
+             behavior: 'smooth'
+           });
+
+}
+function contatos(){
+    vh=300;
+    window.scrollTo({
+            top: vh * window.innerHeight / 100, 
+             behavior: 'smooth'
+           });
+
+}
+
+function redirectWhatsApp() {
+  const whatsappUrl = "https://wa.me/47992884176?text=Olá Leon,";
+   open(whatsappUrl,"_blank"); 
+}
+
